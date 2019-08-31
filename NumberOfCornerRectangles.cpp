bae6a918295c29533084be966a2b683c1de0bd55
Edgar Barboza <edgarbarboza@Edgars-MBP.lan1>
2019-08-31 15:36:53 -0400

    Arbritary

--- NumberOfCornerRectangles.cpp
+++ NumberOfCornerRectangles.cpp
@@ -1,4 +1,4 @@
-/*
+/* 
 Given a grid where each entry is only 0 or 1, find the number of corner rectangles.
 A corner rectangle is 4 distinct 1's on the grid that form an axis-aligned rectangle. 
 Note that only the corners need to have the value 1. Also, all four 1s used must be distinct.
